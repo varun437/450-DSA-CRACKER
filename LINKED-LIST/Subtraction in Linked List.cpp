54165d7bbfd6775a61ae322d537de46cd58fcf28
Malempati Varun <67381413+varun437@users.noreply.github.com>
2021-09-21 15:16:56 +0530

    Create Subtraction in Linked List.cpp

---
+++ LINKED-LIST/Subtraction in Linked List.cpp
@@ -0,0 +1,26 @@
+Node* subLinkedList(Node* l1, Node* l2)
+{
+  /*  Node *temp=NULL;
+    long long num1=0,num2=0;
+    long long mod = 1000000007;
+    while(l1||l2)
+    {
+        if(l1)
+        {
+            num1=((num1*10)+l1->data);
+            l1=l1->next;
+        }
+        if(l2)
+        {
+            num2=((num2*10)+l2->data);
+            l2=l2->next;
+        }
+        
+    }
+    if(num1>num2)
+    temp=new Node((num1-num2));
+    else
+    temp=new Node((num2-num1));
+    
+    return temp;*/
+}
