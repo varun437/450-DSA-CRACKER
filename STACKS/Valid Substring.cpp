df31c96f6cbf1ce74a55bc4c71f332e2ffd56bef
Malempati Varun <67381413+varun437@users.noreply.github.com>
2021-10-05 15:37:33 +0530

    Update Valid Substring.cpp

--- STACKS/Valid Substring.cpp
+++ STACKS/Valid Substring.cpp
@@ -1,26 +1,25 @@
-class Solution {
-  public:
-    int findMaxLen(string s) {
-    
-    stack<int> st;
-    st.push(-1);
-    int result=0;
-    for(int i=0;i<st.size();i++)
-    {
-        if(s[i]=='(')
-        st.push(i);
-        
-        else
-        {
-            st.pop();
-            if(st.empty())
-            st.push(i);
-            else
-            result = max(result,i-st.top());
+
+class Solution{
+public:
+    int maxLength(string S){
+        // code here
+       int ind = -1, maxi = 0;
+        stack<int> st;
+        for(int i = 0;i < S.size();i++){
+            if(S[i] == '(')
+                st.push(i);
+            else{
+                if(!st.empty()){
+                    st.pop();
+                    if(!st.empty())
+                        maxi = max(maxi, i-st.top());
+                    else
+                        maxi = max(maxi, i-ind);
+                }
+                else
+                    ind = i;
+            }
         }
-        
-    }
-    
-    return result;
+        return maxi;
     }
 };
