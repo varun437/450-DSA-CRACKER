30a9b2e9d30bb61e356ff24c2b3e5d34b48943f8
Malempati Varun <67381413+varun437@users.noreply.github.com>
2021-12-07 16:19:30 +0530

    Update Height of Binary Tree.cpp

--- TREES/Height of Binary Tree.cpp
+++ TREES/Height of Binary Tree.cpp
@@ -8,3 +8,20 @@ class Solution{
         return 1+(max(height(node->left),height(node->right)));
     }
 };
+
+              (OR)
+
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+           if(!node)return 0;
+        if(!node->left and !node->right)return 1;
+        int l=INT_MIN,r=INT_MIN;
+        if(node->left)
+            l=height(node->left);
+        if(node->right)
+            r=height(node->right);
+        return max(l,r)+1;
+    }
+};
