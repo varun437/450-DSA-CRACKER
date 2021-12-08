c46604e8d6142f4d66d8726a47e129a325eeeab3
Malempati Varun <67381413+varun437@users.noreply.github.com>
2021-12-08 15:50:11 +0530

    Create Odd even level difference.cpp

---
+++ TREES/Odd even level difference.cpp
@@ -0,0 +1,12 @@
+class Solution{
+  public:
+  
+    /*You are required to complete this function*/
+    int getLevelDiff(Node *root)
+    {
+       if(!root)
+       return 0;
+       
+       return root->data-(getLevelDiff(root->left)+getLevelDiff(root->right));
+    }
+};
